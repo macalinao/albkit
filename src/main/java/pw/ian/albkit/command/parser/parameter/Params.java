a3b67642712f8d0c90a5697dbdbbd1183d0479ee
DziNeIT <o.dzineit@gmail.com>
2014-08-09 13:49:01 +0100

    Add some more methods to Params

--- src/main/java/pw/ian/albkit/command/parser/parameter/Params.java
+++ src/main/java/pw/ian/albkit/command/parser/parameter/Params.java
@@ -1,6 +1,9 @@
 package pw.ian.albkit.command.parser.parameter;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * A wrapper around a HashMap to provide a set of parameters for commands
@@ -13,6 +16,9 @@ public class Params {
      */
     private final Map<String, ParamChatSection> params;
 
+    /**
+     * Whether this set of parameters is valid
+     */
     private boolean valid = true;
 
     /**
@@ -44,10 +50,50 @@ public class Params {
         return params.containsKey(parameter);
     }
 
+    /**
+     * Gets a Set of all of the parameter names contained by this Params
+     * object's Map of parameters
+     *
+     * @return A Set of all of the parameter names for this Params
+     */
+    public Set<String> parameters() {
+        return new HashSet<>(params.keySet());
+    }
+
+    /**
+     * Gets a Set of all of the parameter values contained by this Params
+     * object's Map of parameters
+     *
+     * @return A Set of all of the parameter values for this Params
+     */
+    public Set<ParamChatSection> values() {
+        return new HashSet<>(params.values());
+    }
+
+    /**
+     * Gets a Set of all of the entries to the Map of parameters contained by
+     * this Params object
+     *
+     * @return A Set of all entries to this Params' Map
+     */
+    public Set<Entry<String, ParamChatSection>> entries() {
+        return new HashSet<>(params.entrySet());
+    }
+
+    /**
+     * Checks whether this set of parameters is valid for the base it was
+     * constructed for. Used for automatic validation of arguments
+     *
+     * @return Whether this set of parameters is valid
+     */
     public boolean valid() {
         return valid;
     }
 
+    /**
+     * Invalidates this set of parameters. Should only be used in automatic
+     * validation
+     */
     void invalidate() {
         valid = false;
     }
